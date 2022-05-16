daefc68c1498f1c3af13d3132f4421fa66c8b498
Julien-Allard <julalldev@gmail.com>
2022-05-16 10:32:56 +0200

    Feat: updated readme

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - import and parse all datas
 - modified all headers to be more JS friendly (no spaces and lowercase)
 - exports all results to be called in routes/transactions.js
+- all computing is made in the back so it can send only the useful datas for the dashboard with the transactions route
--- routes/transactions.js
+++ routes/transactions.js
@@ -7,31 +7,17 @@ router.get("/transactions", (req, res) => {
   const uniqueCustomers = []; // contains all unique customers ID for the selected state (or all of them)
   const uniqueOrders = []; // contains all unique orders to compute the total number of unique order (and the revenue/order)
   let totalRevenue;
-  const revenuePerMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const revenuePerMonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // one number for each month of the year
 
   try {
     if (req.query.state) {
-      const stateName =
-        req.query.state.charAt(0).toUpperCase() + req.query.state.slice(1); // to be sure the request state name has an uppercase like in the database
       filteredByState = dataset.results.filter(
-        (transaction) => transaction.state === stateName
+        (transaction) => transaction.state === req.query.state
       );
     } else {
       filteredByState = dataset.results;
     }
 
-    // let january = 0;
-    // let february = 0;
-    // let march = 0;
-    // let april = 0;
-    // let may = 0;
-    // let june = 0;
-    // let july = 0;
-    // let august = 0;
-    // let september = 0;
-    // let october = 0;
-    // let november = 0;
-    // let december = 0;
     for (let i = 0; i < filteredByState.length; i++) {
       // I isolate each unique customer ID to know the exact number of unique customers
       if (!uniqueCustomers.includes(filteredByState[i].customer_id)) {
@@ -43,11 +29,12 @@ router.get("/transactions", (req, res) => {
         uniqueOrders.push(filteredByState[i].order_id);
       }
 
+      // I iterate on the revenuePerMonth array, if the index I iterate on is equal to the Date of the month (ie: january = 0, december = 11) I make a sum of the sales with the corresponding index
       for (let j = 0; j < revenuePerMonth.length; j++) {
         const monthDate = new Date(filteredByState[i].order_date).getMonth();
 
         if (j === monthDate) {
-          revenuePerMonth[j] += Number(filteredByState[i].sales);
+          revenuePerMonth[j] += Math.floor(Number(filteredByState[i].sales));
         }
       }
     }
@@ -60,13 +47,16 @@ router.get("/transactions", (req, res) => {
     totalRevenue = Math.floor(revenue);
 
     const requestedDatas = {
-      revenue: totalRevenue,
-      revenuePerOrder: Number((totalRevenue / uniqueOrders.length).toFixed(2)),
-      customers: uniqueCustomers.length,
+      totalRevenue: totalRevenue,
+      averageRevenuePerOrder: Number(
+        (totalRevenue / uniqueOrders.length).toFixed(2)
+      ),
+      uniqueCustomers: uniqueCustomers.length,
+      revenuePerMonth: revenuePerMonth,
+      allStates: dataset.states.sort(),
     };
 
-    res.status(200).json(filteredByState);
-    console.log(requestedDatas);
+    res.status(200).json(requestedDatas);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
